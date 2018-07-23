1d594636af9c88482c919f317ff5f61f6cddb92c
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2018-07-23 16:12:08 +0800

    Add files via upload

---
+++ inversepairs.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    public static int count=0;
+    public static void main(String []args){
+        int []arr = {9,8,7,6,5,4,3,2,1};
+        int []temp = new int[arr.length];//在排序前，先建好一个长度等于原数组长度的临时数组，避免递归中频繁开辟空间
+        sort(arr,0,arr.length-1,temp);
+        
+        System.out.println(count);
+    }
+    
+    private static void sort(int[] arr,int left,int right,int []temp){
+        if(left<right){
+            
+            int mid = (left+right)/2;
+            
+            sort(arr,left,mid,temp);//左边归并排序，使得左子序列有序
+            
+            sort(arr,mid+1,right,temp);//右边归并排序，使得右子序列有序
+            
+            merge(arr,left,mid,right,temp);//将两个有序子数组合并操作
+        }
+    }
+    
+    private static void merge(int[] arr,int left,int mid,int right,int[] temp){
+        
+        int i = left;//左序列指针
+        int j = mid+1;//右序列指针
+        int t = 0;//临时数组指针
+        
+        while (i<=mid && j<=right){
+            
+            if(arr[i]<=arr[j]){
+                
+                temp[t++] = arr[i++];
+            }
+            
+            // 左数组比右数组大
+            else {
+                  
+                // 数组此时是有序数组,如果a[i]此时比右数组的当前元素a[j]大，那么左数组中a[i]后面的元素就都比a[j]大  
+                
+                count=count+mid-i+1;
+                
+                temp[t++] = arr[j++];
+            }
+        }
+        
+        while(i<=mid){//将左边剩余元素填充进temp中
+            temp[t++] = arr[i++];
+        }
+        
+        while(j<=right){//将右序列剩余元素填充进temp中
+            temp[t++] = arr[j++];
+        }
+        
+        t = 0;
+        //将temp中的元素全部拷贝到原数组中
+        while(left <= right){
+            arr[left++] = temp[t++];
+        }
+    }
+}
